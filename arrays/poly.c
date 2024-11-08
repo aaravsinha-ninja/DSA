e8d3e41f52e499ba351939ab68d306e019a4c6ee
Kaneki.Uzumaki_ <sk5575723@gmail.com>
2024-11-09 02:00:03 +0530

    Create poly.c

---
+++ arrays/poly.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int* add_poly(int *A,int *B,int N, int M)
+{
+    if (N>M)
+    {
+        int *c=(int*)malloc(N+1*sizeof(int));
+        
+    }
+    
+}
+int main()
+{
+    return 0;
+}
