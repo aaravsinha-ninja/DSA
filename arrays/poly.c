0cb754ce4100a60a40c45ff1c8342ca498edc8cd
Kaneki.Uzumaki_ <sk5575723@gmail.com>
2024-11-09 02:19:14 +0530

    Update poly.c

--- arrays/poly.c
+++ arrays/poly.c
@@ -2,12 +2,42 @@
 #include<stdlib.h>
 int* add_poly(int *A,int *B,int N, int M)
 {
+    int *c;
     if (N>M)
     {
-        int *c=(int*)malloc(N+1*sizeof(int));
-        
+        c=(int*)malloc((N+1)*sizeof(int));
+        for (int i = 0; i < N+1; i++)
+        {
+            c[i]=A[i];
+        }
+        for (int i = 0; i < M+1; i++)
+        {
+            c[i]+=B[i];
+        }
     }
-    
+    else
+    {
+        c=(int*)malloc((M+1)*sizeof(int));
+        for (int i = 0; i < M+1; i++)
+        {
+            c[i]=B[i];
+        }
+        for (int i = 0; i < N+1; i++)
+        {
+            c[i]+=A[i];
+        }
+    }
+    return c;
+}
+int* multi_poly(int *A,int *B, int N, int M)
+{
+    int *C = (int*)calloc(N+M+1, sizeof(int));
+    for (int i = 0; i < N+1; i++)
+    {
+        for(int j = 0; j < M+1; j++)
+        C[i+j]+=A[i]+B[j];
+    }
+    return C;
 }
 int main()
 {
